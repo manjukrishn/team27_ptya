4ec42c515068c8e492e44a7ebf09379c265ddd49
Varun <50090823+varun-official@users.noreply.github.com>
2021-07-11 00:04:09 +0530

    ypdated

---
+++ src/RightBar/Rightbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Rightbar.css";
+import Avatar from "@material-ui/core/Avatar";
+
+export default function Rightbar() {
+  return (
+    <div id="rightbar-container">
+      <div className="inside-container">
+        <Avatar alt="profile_image" src="" className="avatar" />
+        <h3 className="text-right">ManjuKrishna</h3>
+        <h3 className="text-right1">User </h3>
+      </div>
+    </div>
+  );
+}
