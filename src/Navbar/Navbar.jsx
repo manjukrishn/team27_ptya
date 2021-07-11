03375e31c2e3baaf4591a5849d38fe81badcc74d
Varun <50090823+varun-official@users.noreply.github.com>
2021-07-11 12:17:15 +0530

    Add files via upload

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,22 +1,12 @@
 import React from "react";
 import ExpandMoreIcon from "@material-ui/icons/ArrowDropDown";
+import ExpandLessIcon from "@material-ui/icons/ArrowDropUp";
+import { useHistory } from "react-router-dom";
+import { isOfficer } from "../utils";
 import "./Navbar.css";
 
 export default function Navbar() {
-  const arr = [
-    "agriculture",
-    "food",
-    "revenue",
-    "education",
-    "health",
-    "transport",
-    "small scale industry",
-    "women and child empowerement",
-    "railway",
-    "finance",
-    "large scale industry",
-    "defence"
-  ];
+  const history = useHistory();
   const items = [
     {
       heading: "Departments",
@@ -38,25 +28,20 @@ export default function Navbar() {
     {
       heading: "Issue status",
       lists: ["solved", "unsolved"]
-    },
-    {
-      heading: "Issue status",
-      lists: ["solved", "unsolved"]
-    },
-    {
-      heading: "Issue status",
-      lists: ["solved", "unsolved"]
-    },
-    {
-      heading: "Issue status",
-      lists: ["solved", "unsolved"]
-    },
-    {
-      heading: "Issue status",
-      lists: ["solved", "unsolved"]
     }
   ];
 
+  const [dropdown, setDropDown] = React.useState({});
+  function collapser(index) {
+    const height = document.getElementById("sil" + index).style.maxHeight;
+    if (height === "0px") {
+      document.getElementById("sil" + index).style.maxHeight = "250px";
+      setDropDown({ ...dropdown, [index]: "250px" });
+    } else {
+      document.getElementById("sil" + index).style.maxHeight = "0px";
+      setDropDown({ ...dropdown, [index]: "0px" });
+    }
+  }
   return (
     <div id="navbar-container">
       <ul>
@@ -66,24 +51,52 @@ export default function Navbar() {
         </li>
         <hr />
         <div id="sidebar-container-item-list">
+          <div style={{ marginLeft: "20px", cursor: "pointer" }}>
+            {isOfficer ? (
+              <table>
+                <tr onClick={() => history.push("/issue-of-your-department")}>
+                  <td>
+                    <h3>#</h3>
+                  </td>
+                  <td>
+                    <h3 className="dept-sidebar">Issues of your department</h3>
+                  </td>
+                </tr>
+              </table>
+            ) : (
+              <table>
+                <tr onClick={() => history.push("/my-issues")}>
+                  <td>
+                    <h3>#</h3>
+                  </td>
+                  <td>
+                    <h3 className="dept-sidebar">My Issues</h3>
+                  </td>
+                </tr>
+              </table>
+            )}
+          </div>
           {items.map((item, index) => {
             return (
               <div>
-                <table style={{marginTop:"20px",marginLeft:"10px"}}>
-                  <tr>
+                <table style={{ marginTop: "20px", marginLeft: "10px" }}>
+                  <tr onClick={() => collapser(index)}>
                     <td>
-                      <ExpandMoreIcon />
+                      {dropdown[index] === "0px" ? (
+                        <ExpandMoreIcon style={{ cursor: "pointer" }} />
+                      ) : (
+                        <ExpandLessIcon style={{ cursor: "pointer" }} />
+                      )}
                     </td>
-                    <td></td>
                     <h3>{item.heading}</h3>
                   </tr>
                 </table>
-                <div id="sidebar-item-list">
+                <div className="sidebar-item-list" id={"sil" + index}>
                   {item.lists.map((it, index) => {
                     return (
                       <li id="items">
                         <table>
-                          <tr>
+                          <tr onClick={() => history.push(`/${it}`)}>
                             <td>#</td>
                             <td>
                               <div className="dept-sidebar">{it}</div>
